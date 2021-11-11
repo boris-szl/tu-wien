4f59f8855f9b3cde7f651227021a36be24bc6828
boris-szl <boris.szelcsanyi@outlook.com>
2021-11-12 00:54:42 +0100

    testing missing, otherwise solved

--- serie05/printUnique.c
+++ serie05/printUnique.c
@@ -8,16 +8,15 @@
 // Last updated. 10/11/2021
 
 void scanVector(int dim, double x[]);
-void printUnique(double x[]);
+void printUnique(int dim, double x[]);
 
 int main() {
 	int n;
-	double a[10]; // Dekalartion eines statistischen Arrays der Länge 10
+	double a[n]; // Dekalartion eines statistischen Arrays der Länge 10
 	printf("Einlesen der Vektorlänge\n");
 	scanf("%d", &n);
 	scanVector(n,a);
-
-
+	printUnique(n,a);
 
 }
 
@@ -29,7 +28,26 @@ void scanVector(int dim, double x[]) {
 }
 
 void printUnique(int dim, double x[]) {
-	int i;
-	int b[dim];
-	for (i=0;i<dim;i++)
+	int i,j,c,k;
+	for (i=0;i<dim;i++) {
+		c=0;
+		for(j=0;j<i;j++) {
+			if (x[i] == x[j]){
+				c++;
+			}
+		}
+		for (k=i+1;k<dim;k++) {
+			if (x[i] == x[k]) {
+				c++;
+			}
+		}
+		if (c==0) {
+			// printing unique element
+			printf("%lf ", x[i]);
+		}
+	}
+	printf("\n");
 }
+
+
+// Testing
