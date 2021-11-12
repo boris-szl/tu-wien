884e36753a89d69e1c4ecbd2eb7ed70b4927b63a
boris-szl <boris.szelcsanyi@outlook.com>
2021-11-12 01:22:12 +0100

    empty

--- serie05/test.c
+++ serie05/test.c
@@ -1,72 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 
+// Author: Boris Szelcsanyi
+// Date created: 10/11/2021
+// Last updated. 10/11/2021
 
 
+int fibonacci(int n, int type);
 
-int main()
+int main() {
 
-{
-    int m, n, row, col, sum = 0, row_ind = 0, col_ind = 0;
-    printf("Einlesen der Anzahl der Zeilen m, Spalten m:\n");
-    scanf("%d %d",&m,&n);
-    int row_arr[m]; // Zeilenarray mit der Länge m
-    int i, j; // Indexvariablen i und j
-    int mat[m][n]; // Deklaration der Matrix
-    // Einlesen der Werte für die Matrix[m][n]
-    for(i = 0; i < m; i++)
-        {
-            for(j = 0; j < n; j++)
-                scanf("%d",&mat[i][j]);
-        }
+    int n;
+    int type;
+    printf("Eingabe einer beliebigen nat. Zahl:\n");
+    scanf("%d", &n);
+    printf("Eingabe des Berechnungsarts: (1 oder 2):\n");
+    scanf("%d", &type);
+    printf("Fibonacci: %d\n", fibonacci(n,type));
 
-    int z = 0; // 
 
-    printf("Sum of rows is: ");
-    for(row=0; row<m; row++)
-        {
-            sum = 0;
-                for(col=0; col<n; col++)
-                    {
-                        sum += mat[row][col];
-                    }
-            printf("%d ",sum);
-            row_arr[z++] = sum;
-        }
-    int temp_row = row_arr[0];
-    for(i=1;i<m;i++)
-        {
-            if(temp_row < row_arr[i])
-                {
-                    temp_row = row_arr[i];
-                    row_ind = i;
-                }
-}
-    printf("\nRow %d has maximum sum with %d", row_ind + 1, temp_row);
-
-    printf("\nSum of columns is ");
-    sum = 0;
-    int y = 0;
-    int col_arr[n];
-    for (i = 0; i < n; ++i)
-        {
-            sum = 0;
-            for (j = 0; j < m; ++j)
-                {
-                    sum = sum + mat[j][i];
-                }       
-            printf("%d ",sum);
-            col_arr[y++] = sum;
-        }
-    int temp_col = col_arr[0];
-    for(i=1;i<n;i++)
-        {
-            if(temp_col < col_arr[i])
-                {
-                    temp_col = col_arr[i];
-                    col_ind = i;
-                }
-        }
-    printf("\nColumn %d has maximum sum\n", col_ind + 1);
     return 0;
 }
+
+
+int fibonacci(int n, int type) {
+    if (n == 0) {
+        return n;
+    } else if (n == 1) {
+        return n;
+    }
+    int folgeglied;
+    if (type == 2) {
+        int t,r;
+        for (int i=0;i<n;i++) {
+            if (i != 0 && i < 3) {
+                t = 1;
+                r = 0;
+            } else if (i > 4) {
+                t = r - t;
+            }
+            r = t + r;
+        }
+        folgeglied = r;
+    } 
+    return folgeglied;
+}
+
