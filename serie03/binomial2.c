0863252fc1a7408ff485fe9d633a57a548ddd37c
boris-szl <boris.szelcsanyi@outlook.com>
2021-12-12 11:51:31 +0100

    removed 's'

--- serie03/binomial2.c
+++ serie03/binomial2.c
@@ -12,8 +12,6 @@ int main()
         } else {
                 printf("Binomialkoeffizient von %d über %d = %d\n", n,k,binomial(n,k));     
         }
-
-
         return 0;
 }
 
@@ -22,5 +20,5 @@ int binomial(int n, int k)
         if(k==0 || k==n) {
                 return 1;
         }
-        return binomial(n-1,k-1) + binomial(n-1,k);s
+        return binomial(n-1,k-1) + binomial(n-1,k);
 }
